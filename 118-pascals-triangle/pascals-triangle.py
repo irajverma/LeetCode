9877eb95400e3e1718a08dc0a1c76d0e276be44c
RAJ VERMA <156664169+irajverma@users.noreply.github.com>
2024-10-27 22:22:46 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (32.07%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
-        if numRows<=1:
-            return [[1]]
-        nums=[[1]]
-        for i in range(1,numRows):
-            newnum=[1]
-            for j in range(len(nums[i-1])-1):
-                newnum.append(nums[i-1][j]+nums[i-1][j+1])
-            newnum.append(1)
-            nums.append(newnum)
-        return nums
+        ans = []
+        for row in range(1, numRows+1):
+            res= 1
+            temp = [res]
+            for col in range(1, row):
+                res = res * (row-col)
+                res = res // col
+                temp.append(res)
+            ans.append(temp)
+        return ans
