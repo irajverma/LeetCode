f3b10ffcc2a957d5478a336fb20c927035119f92
RAJ VERMA <156664169+irajverma@users.noreply.github.com>
2024-10-31 18:03:28 +0530

    Time: 7 ms (86.46%) | Memory: 12.9 MB (42.33%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -5,12 +5,17 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        map1 = []
-        map2 = []
-        for i in s:
-            map1.append(s.index(i))
-        for i in t:
-            map2.append(t.index(i))
-        if map1 == map2:
-            return True
-        return False
+        if len(s) != len(t):
+            return False
+        map_chars = {}
+        set_vals = set()
+        for i in range(len(s)):
+            if s[i] in map_chars:
+                if map_chars[s[i]] != t[i]:
+                    return False
+            else:
+                if t[i] in set_vals:
+                    return False
+                map_chars[s[i]] = t[i]
+                set_vals.add(t[i])
+        return True
