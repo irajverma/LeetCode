d049bc8dbb80a740f8886ab14fd9aeb7c195dc5e
RAJ VERMA <156664169+irajverma@users.noreply.github.com>
2024-11-07 00:25:55 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (14.37%) - LeetSync

--- 1274-number-of-days-between-two-dates/number-of-days-between-two-dates.py
+++ 1274-number-of-days-between-two-dates/number-of-days-between-two-dates.py
@@ -5,29 +5,35 @@ class Solution(object):
         :type date2: str
         :rtype: int
         """
-        month_to_days = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
-
-        y1, m1, d1 = [int(i) for i in date1.strip().split("-")]
-        y2, m2, d2 = [int(i) for i in date2.strip().split("-")]
-
-        def get_days_until_1971(y, m, d):
-            days = d
-            for i in range(1, m):
-                if i == 2 and y % 4 == 0 and y != 2100:
-                    days += 29
-                else:
-                    days += month_to_days[i]
-            
-            for y in range(1971, y):
-                if y % 4 == 0 and y != 2100:
-                    print(y, 366)
-                    days += 366
-                else:
-                    print(y, 365)
-                    days += 365
-            return days
-
-        a = get_days_until_1971(y1, m1, d1)
-        b = get_days_until_1971(y2, m2, d2)
         
-        return b - a if b > a else a - b   
+        def f_date(date): # calculates days passed since '1900-01-01'
+            year0 = '1900'
+            year1, month1, day1 = date.split('-')
+                        
+            days = 0
+            for y in range(int(year0), int(year1)):
+                days += 365
+                if y%100 == 0:
+                    if y%400 == 0:
+                        days += 1
+                else:
+                    if y%4 == 0:
+                        days += 1
+                        
+            for m in range(int(month1)):
+                if m in [1, 3, 5, 7, 8, 10, 12]:
+                    days += 31
+                if m in [4, 6, 9, 11]:
+                    days += 30
+                if m == 2:
+                    days += 28
+                    if int(year1)%100 == 0:
+                        if int(year1)%400 == 0:
+                            days += 1
+                    else:
+                        if int(year1)%4 ==0:
+                            days += 1
+            days += int(day1)
+            return days
+			
+        return abs(f_date(date1) - f_date(date2))
