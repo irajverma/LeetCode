72b789b3b4622c56f32cf383ca9b416b06d3a3e7
RAJ VERMA <156664169+irajverma@users.noreply.github.com>
2024-11-22 23:00:40 +0530

    Time: 223 ms (75.30%) | Memory: 130.5 MB (25.54%) - LeetSync

---
+++ 77-combinations/combinations.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        def backtrack(start, combination):
+            if len(combination) == k:
+                result.append(combination[:])
+                return
+
+            for i in range(start, n + 1):
+                combination.append(i)
+                backtrack(i + 1, combination)
+                combination.pop()
+
+        result = []
+        backtrack(1, [])
+        return result
