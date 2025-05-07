3e53a05510f00ff99d22c0e8bad3f9d8242ff4d2
RAJ VERMA <156664169+irajverma@users.noreply.github.com>
2025-05-07 19:30:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (79.67%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,12 +1,14 @@
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        for i in range(len(digits) - 1, -1, -1):
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
 
-            if digits[i] + 1 != 10:
-                digits[i] += 1
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] = digits[i] + 1
                 return digits
-            
-            digits[i] = 0
-
-            if i == 0:
-                return [1] + digits
+        return [1] + digits
