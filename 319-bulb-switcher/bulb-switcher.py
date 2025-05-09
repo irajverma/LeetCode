02cf814cdbfab1e07b56e28628a3c6dd13de0961
RAJ VERMA <156664169+irajverma@users.noreply.github.com>
2025-05-09 12:33:16 +0530

    Time: 1 ms (6.49%) | Memory: 12.4 MB (54.05%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.py
+++ 319-bulb-switcher/bulb-switcher.py
@@ -1,5 +1,9 @@
-class Solution:
-    def bulbSwitch(self, n: int) -> int:
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
         count = 0
         i = 1
         while i*i <= n:
