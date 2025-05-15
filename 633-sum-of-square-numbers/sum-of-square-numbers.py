676dc29eb64e78a3890ffbf38f9e6762079cb5b3
RAJ VERMA <156664169+irajverma@users.noreply.github.com>
2025-05-15 22:57:50 +0530

    Time: 9 ms (96.15%) | Memory: 12.5 MB (29.12%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def judgeSquareSum(self, c):
+        """
+        :type c: int
+        :rtype: bool
+        """
+        divisor = 2
+        while divisor * divisor <= c:
+            if c % divisor == 0:
+                exponentCount = 0
+                while c % divisor == 0:
+                    exponentCount += 1
+                    c //= divisor
+                if divisor % 4 == 3 and exponentCount % 2 != 0:
+                    return False
+            divisor += 1
+        return c % 4 != 3
