39c5142b82a473dd76e144c356a0eb825b147389
RAJ VERMA <156664169+irajverma@users.noreply.github.com>
2025-06-03 11:52:11 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (43.84%) - LeetSync

---
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,17 @@
+class Solution:
+    def __init__(self):
+        self.prev = None
+
+    def flatten(self, root: TreeNode) -> None:
+        if not root:
+            return
+        
+        self.flatten(root.right)
+
+        self.flatten(root.left)
+
+        root.right = self.prev
+        root.left = None
+
+
+        self.prev = root
