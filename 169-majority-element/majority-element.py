abff109f3139e56bee5b113dab0e4a81494faa4c
RAJ VERMA <156664169+irajverma@users.noreply.github.com>
2025-08-06 12:16:08 +0530

    Time: 7 ms (68.88%) | Memory: 13.7 MB (31.02%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,5 +1,15 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        n = len(nums)
-        return nums[n//2]
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dic = dict()
+        for num in nums:
+            if num in dic:
+                dic[num] += 1
+            else:
+                dic[num] = 1
+        for key, value in dic.items():
+            if value > len(nums)/2:
+                return key
